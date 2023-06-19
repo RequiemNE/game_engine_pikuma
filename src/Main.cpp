0e6607bee8dfea2a48082d7736e7ea7e4f360a82
S-Y <sy-unimportant@outlook.com>
2023-06-19 21:18:39 +0100

    additional config for compilation

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
-int main() {
+int main()
+{
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+
+    sol::state lua2;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
