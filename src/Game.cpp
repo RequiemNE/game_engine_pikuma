0bc5f0f6fa9ba20c21458d12e4bf4340a8d7705d
S-Y <sy-unimportant@outlook.com>
2023-06-21 19:51:34 +0100

    Initial logic for SDL window and renderer.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,5 +1,6 @@
 #include "Game.h"
 #include <iostream>
+#include <SDL2/SDL.h>
 
 Game::Game()
 {
@@ -13,6 +14,32 @@ Game::~Game()
 
 void Game::Initialize()
 {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+    SDL_Window *window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS);
+
+    if (!window)
+    {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+
+    // createRended -1 means get the default render driver. 0 means no SDL flags.
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer)
+    {
+        std::cerr << "Error creating renderer." << std::endl;
+        return;
+    }
 }
 
 void Game::ProcessInput()
