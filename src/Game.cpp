ed69caf3e7991dd222e868213d9a39abe05dc062
S-Y <sy-unimportant@outlook.com>
2023-06-21 13:34:04 +0100

    added Game class for program initialization

---
+++ src/Game.cpp
@@ -0,0 +1,36 @@
+#include "Game.h"
+#include <iostream>
+
+Game::Game()
+{
+    std::cout << "constructor called" << std::endl;
+}
+
+Game::~Game()
+{
+    std::cout << "destructor called" << std::endl;
+}
+
+void Game::Initialize()
+{
+}
+
+void Game::ProcessInput()
+{
+}
+
+void Game::Destroy()
+{
+}
+
+void Game::Render()
+{
+}
+
+void Game::Run()
+{
+}
+
+void Game::Update()
+{
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
-#include <glm/glm.hpp>
-#include <imgui/imgui.h>
-#include <sol/sol.hpp>
+#include "Game.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
+    Game game;
 
-    sol::state lua2;
-
-    SDL_Init(SDL_INIT_EVERYTHING);
-    std::cout << "Hello, world!" << std::endl;
+    game.Initialize();
+    game.Run();
+    game.Destroy();
     return 0;
 }
